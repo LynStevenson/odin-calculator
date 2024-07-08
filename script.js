94bbc434f5ad0bbfcaa4182e46d50186b5708480
Lyn Stevenson <81272840+LynStevenson@users.noreply.github.com>
2024-07-08 15:06:30 -0500

    add keyboard functionality

--- script.js
+++ script.js
@@ -43,6 +43,7 @@ const trimFixed = function(fixed){
 }
 
 //assume operands are strings
+let display = document.querySelector("#output");
 const calculatorDisplay = function(){
     if (leftOperand !== null){
         let leftDisplay;
@@ -67,108 +68,123 @@ const calculatorDisplay = function(){
     }
 }
 
-let display = document.querySelector("#output");
+const operandInput = function(input){
+    if (leftOperand === Infinity){
+        return;
+    }
+    if (operator === null){
+        if (leftOperand === null){
+            if (input === "."){
+                leftOperand = "0.";
+                leftHasDecimal = true;
+            } else {
+                leftOperand = input;
+            }
+        } else {
+            if (input === "." && !leftHasDecimal){
+                leftHasDecimal = true;
+                if (isResult){
+                    leftOperand = display.textContent;
+                }
+                leftOperand += ".";
+                isResult = false;
+            }
+            else if(input !== ".") {
+                if (isResult){
+                    leftOperand = display.textContent;
+                }
+                leftOperand += input;
+                isResult = false;
+            }
+        }
+        calculatorDisplay();
+    } else {
+        if (rightOperand === null){
+            if (input === "."){
+                rightHasDecimal = true;
+                rightOperand = "0.";
+            } else {
+                rightOperand = input;
+            }
+        } else {
+            if(input === "." && !rightHasDecimal){
+                rightHasDecimal = true;
+                rightOperand += ".";
+            } else if (input !== "."){
+               rightOperand += input;
+            }
+        }
+        calculatorDisplay();
+    }
+}
+
+const operatorInput = function(input){
+    if (input === "C"){
+        isResult = false;
+        leftOperand = null;
+        leftHasDecimal = false;
+        rightOperand = null;
+        rightHasDecimal = false;
+        operator = null;
+    } else if (input === "="){
+        if (leftOperand !== null && operator !== null && rightOperand !== null){
+            let result = operate(parseFloat(leftOperand), operator, parseFloat(rightOperand));
+            isResult = true;
+            leftOperand = result.toString();
+            if (leftOperand.includes(".")){
+                leftHasDecimal = true;
+            } else {
+                leftHasDecimal = false;
+            }
+            operator = null;
+            rightOperand = null;
+            rightHasDecimal = false;
+        }
+    } else if (input === document.querySelector(".backspace").textContent || input === "Backspace"){
+        if (rightOperand !== null) {
+            if (rightOperand.slice(-1) === "."){
+                rightHasDecimal = false;
+            }
+            rightOperand = rightOperand.slice(0, -1);
+            if (rightOperand === ""){
+                rightOperand = null;
+            }
+        } else if(operator !== null){
+            operator = null;
+        } else if (leftOperand !== null){
+            if (leftOperand.slice(-1) === "."){
+                leftHasDecimal = false;
+            }
+            leftOperand = leftOperand.slice(0,-1);
+            if (leftOperand === ""){
+                leftOperand = null;
+            }
+            isResult = false;
+        }
+    } else if (leftOperand !== null && rightOperand === null && leftOperand !== Infinity) {
+        operator = input;
+    }
+    calculatorDisplay();
+}
+
 let numberButtons = document.querySelectorAll(".number");
 numberButtons.forEach(function(button){
-    button.addEventListener("click", function(){
-        if (leftOperand === Infinity){
-            return;
-        }
-        if (operator === null){
-            if (leftOperand === null){
-                if (button.classList.contains("decimal")){
-                    leftOperand = "0.";
-                    leftHasDecimal = true;
-                } else {
-                    leftOperand = button.textContent;
-                }
-            } else {
-                if (button.classList.contains("decimal") && !leftHasDecimal){
-                    leftHasDecimal = true;
-                    if (isResult){
-                        leftOperand = display.textContent;
-                    }
-                    leftOperand += ".";
-                    isResult = false;
-                }
-                else if(!button.classList.contains("decimal")) {
-                    if (isResult){
-                        leftOperand = display.textContent;
-                    }
-                    leftOperand += button.textContent;
-                    isResult = false;
-                }
-            }
-            calculatorDisplay();
-        } else {
-            if (rightOperand === null){
-                if (button.classList.contains("decimal")){
-                    rightHasDecimal = true;
-                    rightOperand = "0.";
-                } else {
-                    rightOperand = button.textContent;
-                }
-            } else {
-                if(button.classList.contains("decimal") && !rightHasDecimal){
-                    rightHasDecimal = true;
-                    rightOperand += ".";
-                } else if (!button.classList.contains("decimal")){
-                   rightOperand += button.textContent;
-                }
-            }
-            calculatorDisplay();
-        }
-    })
-})
+    button.addEventListener("click", () => operandInput(button.textContent));
+});
 
 let operations = document.querySelectorAll(".operator");
 operations.forEach(function(button){
-    button.addEventListener("click", function(){
-        if (button.textContent === "C"){
-            isResult = false;
-            leftOperand = null;
-            leftHasDecimal = false;
-            rightOperand = null;
-            rightHasDecimal = false;
-            operator = null;
-        } else if (button.textContent === "="){
-            if (leftOperand !== null && operator !== null && rightOperand !== null){
-                let result = operate(parseFloat(leftOperand), operator, parseFloat(rightOperand));
-                isResult = true;
-                leftOperand = result.toString();
-                if (leftOperand.includes(".")){
-                    leftHasDecimal = true;
-                } else {
-                    leftHasDecimal = false;
-                }
-                operator = null;
-                rightOperand = null;
-                rightHasDecimal = false;
-            }
-        } else if (button.classList.contains("backspace")){
-            if (rightOperand !== null) {
-                if (rightOperand.slice(-1) === "."){
-                    rightHasDecimal = false;
-                }
-                rightOperand = rightOperand.slice(0, -1);
-                if (rightOperand === ""){
-                    rightOperand = null;
-                }
-            } else if(operator !== null){
-                operator = null;
-            } else if (leftOperand !== null){
-                if (leftOperand.slice(-1) === "."){
-                    leftHasDecimal = false;
-                }
-                leftOperand = leftOperand.slice(0,-1);
-                if (leftOperand === ""){
-                    leftOperand = null;
-                }
-                isResult = false;
-            }
-        } else if (leftOperand !== null && rightOperand === null && leftOperand !== Infinity) {
-            operator = button.textContent;
-        }
-        calculatorDisplay();
-    })
+    button.addEventListener("click", () => operatorInput(button.textContent))
+})
+
+document.addEventListener("keydown", function(e){
+    if (e.key.search(/[0-9.]/) !== -1){
+        operandInput(e.key);
+    } else if (e.key.search(/[+\-/*]|Backspace/) !== -1){
+        operatorInput(e.key);
+    } else if (e.key === "c" || e.key === "C") {
+        operatorInput("C");
+    } else if (e.key === "Enter" || e.key === "=") {
+        operatorInput("=");
+    }
 })
